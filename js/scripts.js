7a929df8f02fcca9351fd46ff961af6c552d8137
Alvin Ng and Chip Carnes <alvin@>
2016-06-14 16:31:59 -0700

    Added ticket confirmation

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 var sum = 0;
+var newCart;
 
 function Cart (movie, showtime) {
   this.movie = movie;
@@ -16,15 +17,25 @@ function Ticket (discount) {
 Cart.prototype.totalCost = function() {
   for(i = 0; i < this.tickets.length; i++) {
     sum += this.tickets[i];
+  } if(this.movie !== "Finding Dory") {
+    sum *=.90;
+  } if(this.showtime !== '5:00pm' && this.showtime !=='7:00pm' && this.showtime !=='9:00') {
+    sum *=.90;
   }
   this.totalPrice = sum;
-  console.log(this.totalPrice);
+  sum = 0;
 };
 
 Ticket.prototype.priceCalc = function() {
   this.discount = this.discount * 15;
   return this.discount;
 };
+
+function resetFields() {
+    $("select#movie-select").val("");
+    $("select#time-select").val("");
+    $("select.age-select").val("");
+}
 
 // var showtimes = ["9:00am", "11:00am", "1:00pm", "3:00pm", "5,00pm", "7:00pm","9:00pm"];
 // var movies = ["Finding Dory", "Zootopia", "Frozen", "The Good Dinosaur"];
@@ -56,10 +67,25 @@ $(document).ready(function(){
       newCart.tickets.push(newTicket);
     });
 
-    console.log(newCart);
+    resetFields();
     newCart.totalCost();
-    console.log(newCart);
+    $("#output").show();
+    if (newCart.movie === "Finding Dory") {
+      $("#dory").show(this);
+      $("h2").text(newCart.movie);
+      $(".totalCost").text("$" + newCart.totalPrice);
+    } else if (newCart.movie === "Zootopia") {
+      $("#zootopia").show(this);
+      $("h2").text(newCart.movie);
+      $(".totalCost").text("$" + newCart.totalPrice);
+    } else if (newCart.movie === "Frozen") {
+      $("#frozen").show(this);
+      $("h2").text(newCart.movie);
+      $(".totalCost").text("$" + newCart.totalPrice);
+    } else if (newCart.movie === "The Good Dinosaur") {
+      $("#dinosaur").show(this);
+      $("h2").text(newCart.movie);
+      $(".totalCost").text("$" + newCart.totalPrice);
+    }
   });
 });
-
-// var ticketAmount = parseInt($("form#num-tickets").val());
